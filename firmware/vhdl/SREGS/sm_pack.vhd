d4b0019327ea7a8dfb568014784f8e9d9df9cdb0
Andre Goerres <a.goerres@fz-juelich.de>
2014-05-14 15:20:58 +0200

    Changed some settings in Xilinx project.

--- firmware/vhdl/SREGS/sm_pack.vhd
+++ firmware/vhdl/SREGS/sm_pack.vhd
@@ -81,17 +81,17 @@ package sample_package is
 
 
 	constant GLS_TIMER				: integer	:=16#08#; 	--hex20
-	
+
 -- Adressbereich fuer MMCM_DRP
-	constant SM_DRP					: integer	:=16#20#;	--  to 16#3F#, 32 regs	
+	constant SM_DRP					: integer	:=16#20#;	--  to 16#3F#, 32 regs
 
 -- ======================================================================= --
 -- constants for the regs                              --
 -- ======================================================================= --
 
-	constant RB_BUSY_REG 			: integer	:=16#104#; -- hex address 410 
+	constant RB_BUSY_REG 			: integer	:=16#104#; -- hex address 410
 	constant RB_DCMDATA_REG			: integer	:=16#105#;	--hex address 414
-	
+
 --	constant TPX_FIFODUMMY_REG  	: T_MGLREG	:="10101";	--hex address 854 (adresse wurde geaendert, zurueckaendern! )
 	constant TPX_FIFODUMMY_REG  	: integer	:=16#108#;  --hex address 420
 	constant TPX_LEDINFOREG			: integer	:=16#107#;  --hex address 41c
@@ -113,38 +113,38 @@ package sample_package is
 	constant TPXMASTER_READTE	 	: integer	:=16#12e#;	--hex address 4B8
 	constant TPXMASTER_TRIGGERLO 	: integer	:=16#12f#;	--hex address 4BC
 	constant TPXMASTER_TRIGGERHI 	: integer	:=16#130#;	--hex address 4C0
-	constant TPXMASTER_BITCOUNT 	: integer	:=16#131#;	--hex address 4C4	
-	
-	constant LED_REG				   : integer 	:=16#132#; 	--hex address 4C8 
-	
-	constant LCD_MODE_REG			: integer 	:=16#133#; 	--hex address 4CC 
-	constant LCD_FIFO_CHAR_REG		: integer 	:=16#134#; 	--hex address 4D0 
---	constant LCD_CHAR_REG_1_1		: integer 	:=16#134#; 	--hex address 4D0 
+	constant TPXMASTER_BITCOUNT 	: integer	:=16#131#;	--hex address 4C4
+
+	constant LED_REG				   : integer 	:=16#132#; 	--hex address 4C8
+
+	constant LCD_MODE_REG			: integer 	:=16#133#; 	--hex address 4CC
+	constant LCD_FIFO_CHAR_REG		: integer 	:=16#134#; 	--hex address 4D0
+--	constant LCD_CHAR_REG_1_1		: integer 	:=16#134#; 	--hex address 4D0
 --	constant LCD_CHAR_REG_1_2		: integer 	:=16#135#; 	--hex address 4D4
 --	constant LCD_CHAR_REG_1_3		: integer 	:=16#136#; 	--hex address 4D8
 --	constant LCD_CHAR_REG_1_4		: integer 	:=16#137#; 	--hex address 4DC
---	constant LCD_CHAR_REG_2_1		: integer 	:=16#138#; 	--hex address 4E0 
+--	constant LCD_CHAR_REG_2_1		: integer 	:=16#138#; 	--hex address 4E0
 --	constant LCD_CHAR_REG_2_2		: integer 	:=16#139#; 	--hex address 4E4
 --	constant LCD_CHAR_REG_2_3		: integer 	:=16#13a#; 	--hex address 4E8
---	constant LCD_CHAR_REG_2_4		: integer 	:=16#13b#; 	--hex address 4EC      
-	
+--	constant LCD_CHAR_REG_2_4		: integer 	:=16#13b#; 	--hex address 4EC
+
 --	constant SM_IDENT				: integer :=16#101#;  	-- 404   raus, doppelt zu GLS_IDENT
-	
+
 	constant SCR_ENA				: integer := 0;
 	constant SCR_HIGH				: integer := 15;
 
-	constant SM_SR					: integer :=16#109#;    --
+	constant SM_SR						: integer :=16#109#;    -- 424
 	constant SM_BLK_SIZE			: integer :=16#106#;    -- 418    DMA FIFO data count
 	constant SM_RO_DATA				: integer :=16#100#;    -- 400    Register address for DMA transfer
 	constant SM_RO_DATA_COUNT	: integer :=16#10a#;
 -- -------------------------- Giga Link protocol constants --------------- --
 
-	constant GL_START	: T_SLV8 := x"1C";	-- K.28.0  
-	constant GL_XON		: T_SLV8 := x"5C";	-- K.28.2  
-	constant GL_XOFF	: T_SLV8 := x"7C";	-- K.28.3  
-	constant GL_RESET	: T_SLV8 := x"9C";	-- K.28.4  
-	constant GL_IDLE	: T_SLV8 := x"BC";	-- K.28.5  
-	constant GL_SYNC	: T_SLV8 := x"DC";	-- K.28.6  
+	constant GL_START	: T_SLV8 := x"1C";	-- K.28.0
+	constant GL_XON		: T_SLV8 := x"5C";	-- K.28.2
+	constant GL_XOFF	: T_SLV8 := x"7C";	-- K.28.3
+	constant GL_RESET	: T_SLV8 := x"9C";	-- K.28.4
+	constant GL_IDLE	: T_SLV8 := x"BC";	-- K.28.5
+	constant GL_SYNC	: T_SLV8 := x"DC";	-- K.28.6
 
 end sample_package;
 
